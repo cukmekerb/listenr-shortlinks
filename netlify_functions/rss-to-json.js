e2ce8f30096d83ce5f5036a43a7d0a0946b661bf
cukmekerb <cukmekerb+git@gmail.com>
2021-03-24 13:01:35 -0700

    added length parameters

--- netlify_functions/rss-to-json.js
+++ netlify_functions/rss-to-json.js
@@ -3,15 +3,24 @@ exports.handler = async (event, context) => {
     var rssurl = decodeURIComponent(event.queryStringParameters.url);
     if (!rssurl) return { statusCode: 404, body: `{"error": "There was no provided RSS URL"}`, headers: { "content-type": "application/json", "access-control-allow-origin": "*" } }
     try {
+        var res_len = Number(event.queryStringParameters.items) || 50; // how many results to give
+        var start_from = Number(event.queryStringParameters.startfrom) || 0; // where to start from
         var json_feed = await Feed.load(rssurl);
-        json_feed = JSON.stringify(json_feed)
+        var new_items = json_feed.items.splice(start_from, res_len);
+        var new_feed = JSON.parse(JSON.stringify(json_feed))
+        new_feed.items = new_items;
+        new_feed = JSON.stringify(new_feed)
+        // allitems param
+        if(event.queryStringParameters.allitems == 1 || event.queryStringParameters.allitems == "true"){
+            new_feed = JSON.stringify(json_feed)
+        }
         return {
             headers: {
                 "content-type": "application/json",
                 "access-control-allow-origin": "*"
             },
             statusCode: 200,
-            body: json_feed
+            body: new_feed
         }
     }
     catch (err) {
